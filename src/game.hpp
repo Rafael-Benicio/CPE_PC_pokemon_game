675349e156340e6dfd1f09b5ea63b9985a4cf1e7
Rafael-Benicio <jerobenicio@gmail.com>
2024-12-11 14:15:12 -0300

    Fix: Adiciona frame rate macro esquecida

--- src/game.hpp
+++ src/game.hpp
@@ -1,13 +1,19 @@
 #if !defined(SCREEN_WIDTH) && !defined(SCREEN_HEIGHT)
 
-#define SCREEN_WIDTH 240
-#define SCREEN_HEIGHT 160
+#define SCREEN_WIDTH 240 * 4
+#define SCREEN_HEIGHT 160 * 4
 
 #endif
 
 #if !defined(TILE_SIZE) && !defined(BLOCK_SIZE)
 
-#define TILE_SIZE 8
-#define BLOCK_SIZE 16
+#define TILE_SIZE 8 * 4
+#define BLOCK_SIZE 16 * 4
+
+#endif
+
+#ifndef FRAME_HATE
+
+#define FRAME_HATE 60
 
 #endif
